f63f5b5f8e86d8cb09f0a879363b86d1fb68655b
YaelRevayev <gexploreapp@gmail.com>
2024-03-21 11:06:26 +0200

    optimized logs by date time

--- http_client.py
+++ http_client.py
@@ -2,12 +2,39 @@
 import os
 import requests
 import logging
-
-log_file = "http_client_log.txt"
-logging.basicConfig(filename=log_file, level=logging.INFO)
+from datetime import datetime
 
 
-def send_files_to_server(source_dir, server_url):
+log_dir = "logs"
+if not os.path.exists(log_dir):
+    os.makedirs(log_dir)
+
+
+def create_error_logger():
+    error_logger = logging.getLogger("error_logger")
+    error_logger.setLevel(logging.ERROR)
+    error_handler = logging.FileHandler(os.path.join(log_dir, "error_logs.txt"))
+    error_formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+    error_handler.setFormatter(error_formatter)
+    error_logger.addHandler(error_handler)
+    return error_logger
+
+
+def create_info_logger():
+    info_logger = logging.getLogger("info_logger")
+    info_logger.setLevel(logging.INFO)
+    info_handler = logging.FileHandler(
+        os.path.join(log_dir, f"client_logs_{datetime.now().strftime('%Y-%m-%d')}.txt")
+    )
+    info_formatter = logging.Formatter(
+        "%(asctime)s - %(filename)s - %(levelname)s - %(message)s"
+    )
+    info_handler.setFormatter(info_formatter)
+    info_logger.addHandler(info_handler)
+    return info_logger
+
+
+def send_files_to_server(source_dir, server_url, info_logger, error_logger):
     files_to_send = []
 
     for file_name in os.listdir(source_dir):
@@ -16,17 +43,20 @@ def send_files_to_server(source_dir, server_url):
             with open(file_path, "rb") as file:
                 file_data = file.read()
                 files_to_send.append(("files", (file_name, file_data)))
+                info_logger.info(f"Processing file: {file_name}")
 
     response = requests.post(server_url, files=files_to_send)
 
     if response.status_code == 200:
-        logging.info("Files were sent successfully.")
+        info_logger.info("Files were sent successfully.")
     else:
-        logging.error(f"Failed to send files. Status code: {response.status_code}")
+        error_logger.error(f"Failed to send files. Status code: {response.status_code}")
     return response
 
 
 if __name__ == "__main__":
     source_dir = "task_azure_output"
     server_url = "http://172.212.97.195/upload/"
-    send_files_to_server(source_dir, server_url)
+    send_files_to_server(
+        source_dir, server_url, create_info_logger(), create_error_logger()
+    )
