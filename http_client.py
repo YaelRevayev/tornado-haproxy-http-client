9280a4f5224836704dfc9585367358905ea4205d
YaelRevayev <gexploreapp@gmail.com>
2024-03-21 10:27:53 +0200

    loggings chnaged

--- http_client.py
+++ http_client.py
@@ -3,19 +3,19 @@ import os
 import requests
 import logging
 
+log_file = "http_client_log.txt"
+logging.basicConfig(filename=log_file, level=logging.INFO)
 
-logging.basicConfig(level=logging.INFO)
 
 def send_files_to_server(source_dir, server_url):
     files_to_send = []
 
     for file_name in os.listdir(source_dir):
         file_path = os.path.join(source_dir, file_name)
-        print(file_path)
         if os.path.isfile(file_path):
-            with open(file_path, 'rb') as file:
+            with open(file_path, "rb") as file:
                 file_data = file.read()
-                files_to_send.append(('files', (file_name, file_data)))
+                files_to_send.append(("files", (file_name, file_data)))
 
     response = requests.post(server_url, files=files_to_send)
 
@@ -25,7 +25,8 @@ def send_files_to_server(source_dir, server_url):
         logging.error(f"Failed to send files. Status code: {response.status_code}")
     return response
 
+
 if __name__ == "__main__":
-    source_dir = 'task_azure_output'
-    server_url = 'http://172.212.97.195/upload/'
+    source_dir = "task_azure_output"
+    server_url = "http://172.212.97.195/upload/"
     send_files_to_server(source_dir, server_url)
